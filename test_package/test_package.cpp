1ff25262f75f0b3c598b950f3e78e1f214d3be4e
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-13 17:38:00 +0100

    add test_package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,3 +1,26 @@
+#include <GLTFSDK/GLTF.h>
+#include <GLTFSDK/Deserialize.h>
+#include <GLTFSDK/Document.h>
+#include <GLTFSDK/Math.h>
+#include <GLTFSDK/Serialize.h>
+
+#include <utility>
+
 int main() {
+  Microsoft::glTF::Document originalDoc;
+
+  Microsoft::glTF::Scene sc;
+  sc.id = "0";
+  sc.nodes = { "0" };
+  originalDoc.SetDefaultScene(std::move(sc));
+
+  Microsoft::glTF::Node matrixNode;
+  matrixNode.id = "0";
+  matrixNode.name = "matrixNode";
+  matrixNode.matrix = Microsoft::glTF::Matrix4::IDENTITY;
+  originalDoc.nodes.Append(std::move(matrixNode));
+
+  auto outputString = Microsoft::glTF::Serialize(originalDoc);
+  auto twoPassDoc = Microsoft::glTF::Deserialize(outputString);
   return 0;
 }
