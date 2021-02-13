7787cb0c044374f1935b3b4d2a876c2877b9219f
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-02-13 15:20:16 +0100

    fix build and installation

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class GltfSdkConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     _cmake = None
 
@@ -32,23 +32,28 @@ class GltfSdkConan(ConanFile):
             del self.options.fPIC
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, 14)
-        if self.settings.os != "Windows":
-            raise ConanInvalidConfiguration("Windows only")
 
     def requirements(self):
-        self.requires("rapidjson/cci.20200410")
+        self.requires("rapidjson/1.1.0")
+
+    def build_requirements(self):
+        if not (tools.which("pwsh") or tools.which("powershell")):
+            raise ConanInvalidConfiguration("powershell recipe not yet available in CCI")
+            self.build_requires("powershell/7.1.2")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename("glTF-SDK-r" + self.version, self._source_subfolder)
 
     def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                              "if (WIN32)", "if (MSVC)")
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                               "add_subdirectory(External/RapidJSON)", "")
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                               "add_subdirectory(External/googletest)", "")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "GLTFSDK", "CMakeLists.txt"),
-                              "target_link_libraries(GLTFSDK\n    RapidJSON\n)", "")
 
     def _configure_cmake(self):
         if self._cmake:
